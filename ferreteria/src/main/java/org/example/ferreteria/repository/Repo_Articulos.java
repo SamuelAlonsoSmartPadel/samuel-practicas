c90d370b065cc5aaca9d541a88a7f02f8b444bf2
Samuel Alonso <alonsomanolez@gmail.com>
2026-03-24 15:37:02 +0100

    proyecto zara

--- ferreteria/src/main/java/org/example/ferreteria/repository/Repo_Articulos.java
+++
@@ -1,9 +0,0 @@
-package org.example.ferreteria.repository;
-import org.example.ferreteria.entity.Articulos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Repo_Articulos extends JpaRepository<Articulos, Long> {
-
-}
